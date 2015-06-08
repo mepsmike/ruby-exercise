1bf7640f4b431c16e23c6dbca2e464862bc7cf40
Mike.C <mepsmike@gmail.com>
2015-06-08 17:09:26 +0800

    basic4

--- basic3.rb
+++ basic3.rb
@@ -1,3 +1,9 @@
 movielist = {mov1:1975,mov2:2004,mov3:2013,mov4:2001,mov5:1981}
 
 movielist.each{|key,value| puts "#{value}"}
+
+year_array=[]
+
+movielist.each{|key,value| year_array.push(value)}
+
+year_array.each{|x| puts x}
