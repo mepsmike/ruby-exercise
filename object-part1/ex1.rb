5bda4f1e565c24ea13c6937be1a2873983eecc72
Mike.C <mepsmike@gmail.com>
2015-06-09 11:47:34 +0800

    object-part1/ex2

--- object-part1/ex1.rb
+++ object-part1/ex1.rb
@@ -1,6 +1,8 @@
 class MyCar
 
-	attr_accessor :speed
+	attr_accessor :speed ,:color
+
+	attr_reader :year
 
 	def initialize(year,color,model,speed)
 		@year = year
@@ -30,9 +32,25 @@ class MyCar
 		puts "speed set to zero speed now:#{speed}"
 	end
 
+	def change_color(color)
+
+		@color=color
+
+		puts "the color change to #{@color}"
+
+	end
+
+	def view_year
+
+		puts "the year is #{@year}"
+
+	end
+
 end
 
 car = MyCar.new(1976,"black","fly",0)
 car.speed_up(20)
 car.brake(10)
 car.shut
+car.change_color("white")
+car.view_year
